f0b23272d480c0e9b727a9d83715cbc6016f2cc1
mngerasimenko <mngerasimenko@gmail.com>
2023-08-18 15:35:17 +0400

    app config

--- src/main/java/ru/mngerasimenko/employee/rest/configuraton/MyWebInitializer.java
+++ src/main/java/ru/mngerasimenko/employee/rest/configuraton/MyWebInitializer.java
@@ -1,2 +1,18 @@
-package ru.mngerasimenko.employee.rest.configuraton;public class MyWebInitializer {
+package ru.mngerasimenko.employee.rest.configuraton;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
