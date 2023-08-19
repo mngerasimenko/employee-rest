6977b52cfcdbb7c97bb5068581e10d7b24b984fb
mngerasimenko <mngerasimenko@gmail.com>
2023-08-19 12:19:09 +0400

    EmployeeGlobalExceptionHandler

--- src/main/java/ru/mngerasimenko/employee/rest/controller/MyRESTController.java
+++ src/main/java/ru/mngerasimenko/employee/rest/controller/MyRESTController.java
@@ -1,11 +1,8 @@
 package ru.mngerasimenko.employee.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.mngerasimenko.employee.rest.entity.Employee;
-import ru.mngerasimenko.employee.rest.exception_handling.EmployeeIncorrectData;
 import ru.mngerasimenko.employee.rest.service.EmployeeService;
 
 import java.util.List;
@@ -34,21 +31,5 @@ public class MyRESTController {
 		}
 
 		return employee;
-	}
-
-	@ExceptionHandler
-	public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchFieldException exception) {
-		EmployeeIncorrectData data = new EmployeeIncorrectData();
-		data.setInfo(exception.getMessage());
-
-		return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-	}
-
-	@ExceptionHandler
-	public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
-		EmployeeIncorrectData data = new EmployeeIncorrectData();
-		data.setInfo(exception.getMessage());
-
-		return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
 	}
 }
